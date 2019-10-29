ef58a3bd1715e57a166cd8d3a87ce308161a6449
taterntots <matthew.a.weidner@gmail.com>
2019-10-29 18:08:12 -0400

    hit MVP. Just trying to get some other listeners working.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 //1. Resizing the window
 const resizeWin = document.querySelector("body");
 // console.log(resizeWin);
-window.addEventListener("resize", doneResizing((e) => {
+window.addEventListener("resize", doneResizing((event) => {
     document.body.textContent = 'oh no...';
     document.body.style.fontSize = "400px";
+    event.stopPropagation();
 }));
 
 function doneResizing(func){
@@ -20,12 +21,14 @@ const hoverImg = document.querySelectorAll("img");
 hoverImg.forEach(element => {
     element.addEventListener("mouseover", () => {
         element.src = 'https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+        event.stopPropagation();
     })
 })
 
 hoverImg.forEach(element => {
     element.addEventListener("mouseout", () => {
         element.src = 'https://images.unsplash.com/photo-1509557965875-b88c97052f0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+        event.stopPropagation();
     })
 })
 
@@ -48,7 +51,7 @@ changeLogo.addEventListener("dblclick", () => {
 const spiderText = document.querySelectorAll("p");
 spiderText.forEach(element => {
     element.addEventListener("select", () => {
-        element.textContent = '🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️';
+        element.textContent = 'poot';
         element.style.fontSize = "30px";
     })
 })
@@ -72,7 +75,7 @@ colorTheme.addEventListener("click", () => {
 const spiderSquish = document.querySelectorAll("p");
 spiderSquish.forEach(element => {
     if (element.textContent === '🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️🕷️') {
-        element.addEventListener('keydown', () => {
+        window.addEventListener('keydown', () => {
         element.textContent = "fart";
         document.body.style.backgroundColor="orange";
         console.log(element.textContent);
@@ -88,4 +91,29 @@ dragImg.forEach(element => {
     })
 })
 
-//10.
+//10. Changes the first h2 header to a different string
+const headerSwap = document.querySelector(".intro h2");
+console.log(headerSwap);
+window.addEventListener("keydown", () => {
+    headerSwap.textContent = "What a Horrible Night to Have a Curse";
+})
+
+//11. Plays a sound when the signUp button is clicked
+const sound = new Audio('audio/LTTP_Secret.wav');
+const playJingle = document.querySelectorAll(".btn");
+playJingle.forEach(element => {
+    element.addEventListener("auxclick", () => {
+        sound.play();
+    })
+})
+
+//12. Defaults the navigation elements to not refresh the page
+const stopLinks = document.querySelectorAll("nav a");
+console.log(stopLinks);
+stopLinks.forEach(element => {
+    element.addEventListener("click", (event) => {
+        event.preventDefault();
+    })
+})
+
+//13. 
